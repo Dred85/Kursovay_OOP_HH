f5c12fa1f96594a40ae30b81001526ed9f6129a6
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-30 08:54:12 +0300

    fix main

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from src.getting_and_working_with_vacancies import HHAPI
 from src.add_vacancies_in_json import WorkWithJSON
 
 
-def hello():
+def main():
     # Обращения к пользователю
-    profession = input("Введите профессию для поиска ")
-    quantity_profession = int(input("Какое количество вакансий с наибольшей зарплатой вы хотите увидеть?"))
+    profession = input("Введите профессию для поиска: ")
+    quantity_profession = int(input("Какое количество вакансий с наибольшей зарплатой вы хотите увидеть? "))
 
     # Указываю ссылку на API HH
     hh_url = HHAPI('https://api.hh.ru/vacancies')
@@ -20,29 +20,32 @@ def hello():
     WorkWithJSON.add_json(vacancies)
 
     # Спрашиваю у пользователя вывести ли топ вакансий в количестве которое он указал?
-    answer_1 = input('Хотите увидеть вакансии с наибольшей зарплатой?')
-    if answer_1 == 'да':
+    answer_1 = input('Хотите увидеть вакансии с наибольшей зарплатой (Да/Нет)? ')
+    if answer_1.lower() == 'да':
         # Вывожу отсортированные вакансии в том количестве которое попросил пользователь
         pprint(vacancies_sorted[:quantity_profession])
 
     # Спрашиваю у пользователя хочет ли он отсортировать вакансии
-    answer_2 = input('Отсортируем вакансии?')
-    if answer_2 == 'да':
+    answer_2 = input('Отсортируем вакансии (Да/Нет)? ')
+    if answer_2.lower() == 'да':
         # Спрашиваю у пользователя по чему будем сортировать
-        answer_3 = input('Сортируем по названию или по требованиям?')
-        if answer_3 == 'по названию':
+        answer_3 = input('Сортируем по названию или по требованиям (н/т)? ')
+        if answer_3.lower() == 'н':
             # Спрашиваю у пользователя название вакансии для сортировки
-            answer_4 = input('Введите ключевое слово в названии вакансии')
+            answer_4 = input('Введите ключевое слово в названии вакансии: ')
             WorkWithJSON.get_info_json_name(answer_4)
-        elif answer_3 == 'по требованиям':
-            answer_5 = input('Введите ключевое слово в описании вакансии')
+        elif answer_3 == 'т':
+            answer_5 = input('Введите ключевое слово в описании вакансии: ')
             WorkWithJSON.get_info_json_requirements(answer_5)
-    answer_6 = input('Введите диапазон зарплат через -(10000-100000)')
+    answer_6 = input('Введите диапазон зарплат через -(10000-100000): ')
     answer_6_split = answer_6.split('-')
     for salary in WorkWithJSON.filtered_information:
         if int(salary['salary_from']) > int(answer_6_split[0]):
             print(f'{salary['name']}, {salary['link_to_vacancy']}, {salary['salary_from']}, {salary['salary_to']}, '
                   f'{salary['requirements']}')
 
+if __name__ == '__main__':
+    # Вызываю функцию main()
+    main()
 
-hello()
+
