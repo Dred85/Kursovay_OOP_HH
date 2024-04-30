f254de098b16ac20c7449983207be54f4923585f
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-04-30 08:25:29 +0300

    add.py

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-hh_api = HeadHunterAPI()
+from pprint import pprint
 
-# Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies("Python")
-
-# Преобразование набора данных из JSON в список объектов
-vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
-
-# Пример работы контструктора класса с одной вакансией
-vacancy = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.delete_vacancy(vacancy)
-
-# Функция для взаимодействия с пользователем
-def user_interaction():
-    platforms = ["HeadHunter"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    salary_range = input("Введите диапазон зарплат: ") # Пример: 100000 - 150000
-
-    filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-
-    ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-
-    sorted_vacancies = sort_vacancies(ranged_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+from src.getting_and_working_with_vacancies import HHAPI
+from src.add_vacancies_in_json import WorkWithJSON
 
 
-if __name__ == "__main__":
-    user_interaction()
+def hello():
+    # Обращения к пользователю
+    profession = input("Введите профессию для поиска ")
+    quantity_profession = int(input("Какое количество вакансий с наибольшей зарплатой вы хотите увидеть?"))
+
+    # Указываю ссылку на API HH
+    hh_url = HHAPI('https://api.hh.ru/vacancies')
+
+    # Получаю вакансии и сортирую их
+    vacancies = hh_url.get_info(profession)
+    vacancies_sorted = sorted(vacancies, reverse=True)
+
+    # Записываю все вакансии в JSON файл
+    WorkWithJSON.add_json(vacancies)
+
+    # Спрашиваю у пользователя вывести ли топ вакансий в количестве которое он указал?
+    answer_1 = input('Хотите увидеть вакансии с наибольшей зарплатой?')
+    if answer_1 == 'да':
+        # Вывожу отсортированные вакансии в том количестве которое попросил пользователь
+        pprint(vacancies_sorted[:quantity_profession])
+
+    # Спрашиваю у пользователя хочет ли он отсортировать вакансии
+    answer_2 = input('Отсортируем вакансии?')
+    if answer_2 == 'да':
+        # Спрашиваю у пользователя по чему будем сортировать
+        answer_3 = input('Сортируем по названию или по требованиям?')
+        if answer_3 == 'по названию':
+            # Спрашиваю у пользователя название вакансии для сортировки
+            answer_4 = input('Введите ключевое слово в названии вакансии')
+            WorkWithJSON.get_info_json_name(answer_4)
+        elif answer_3 == 'по требованиям':
+            answer_5 = input('Введите ключевое слово в описании вакансии')
+            WorkWithJSON.get_info_json_requirements(answer_5)
+    answer_6 = input('Введите диапазон зарплат через -(10000-100000)')
+    answer_6_split = answer_6.split('-')
+    for salary in WorkWithJSON.filtered_information:
+        if int(salary['salary_from']) > int(answer_6_split[0]):
+            print(f'{salary['name']}, {salary['link_to_vacancy']}, {salary['salary_from']}, {salary['salary_to']}, '
+                  f'{salary['requirements']}')
+
+
+hello()
